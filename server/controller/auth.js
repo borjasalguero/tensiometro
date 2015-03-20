a7f62cc2f4f8e72165afb0d487c3c791c88c9dd4
borjasalguero <fbsc@tid.es>
2015-03-20 18:23:55 +0000

    DRAFT: Codigo limpio y funcionando

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -3,38 +3,37 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var User = require('../models/user');
 
-
+// Necesario para serializar la sesion. Si lo quito NO funciona
 passport.serializeUser(function(user, done) {
+  console.log('Serializando USUARIO');
   done(null, user);
 });
-
+// Igual
 passport.deserializeUser(function(user, done) {
-  console.log('********** VAMOS a DESERIALIZAR!');
+  console.log('*****************');
+  console.log(JSON.stringify(user));
+  console.log('*****************');
+  console.log('DES-serializando USUARIO');
   done(null, user);
 });
-
+// En el caso de estar en un LOGIN, acudiremos a esta funcion
+// para consultar en la BBDD si el usuario existe, y si las credenciales
+// son válidas
 passport.use(new LocalStrategy(
   function(username, password, done) {
-    console.log('************* isAuthenticated');
     console.log('username: ' + username);
     console.log('password: ' + password);
-
-
-    // passport.deserializeUser(function(user, done) {
-    //   console.log('********** VAMOS a DESERIALIZAR!');
-    //   done(null, user);
-    // });
-
 
     User.findOne({ username: username }, function(err, user) {
       if (err) {
         return done(err);
       }
-
+      // Si no hay usuario dado el username....
       if (!user) {
         return done(null, false, { message: 'Incorrect username.' });
       }
-
+      // Si hay usuario, comparamos las contraseñas (convertidas)
+      // a HASH
       user.verifyPassword(password, function(err, isMatch) {
         if (err || !isMatch) {
           return done(null, false, { message: 'Incorrect password.' });
@@ -46,56 +45,6 @@ passport.use(new LocalStrategy(
   }
 ));
 
+// Session está a TRUE ya que queremos usar sesiones. Si lo ponemos a FALSE
+// necesitaremos añadir username/password en cada request
 exports.isAuthenticated = passport.authenticate('local', { session : true });
-
-
-
-// exports.isAuthenticated = function() {
-
-//   console.log('vamos que nos vamos');
-
-//   passport.serializeUser(function(user, done) {
-//     done(null, user);
-//   });
-
-//   passport.deserializeUser(function(user, done) {
-//     console.log('********** VAMOS a DESERIALIZAR!');
-//     done(null, user);
-//   });
-
-
-
-
-//   passport.use(new LocalStrategy(
-//     function(username, password, done) {
-//       console.log('************* isAuthenticated');
-//       console.log('username: ' + username);
-//       console.log('password: ' + password);
-
-
-//       // passport.deserializeUser(function(user, done) {
-//       //   console.log('********** VAMOS a DESERIALIZAR!');
-//       //   done(null, user);
-//       // });
-
-
-//       User.findOne({ username: username }, function(err, user) {
-//         if (err) {
-//           return done(err);
-//         }
-
-//         if (!user) {
-//           return done(null, false, { message: 'Incorrect username.' });
-//         }
-
-//         user.verifyPassword(password, function(err, isMatch) {
-//           if (err || !isMatch) {
-//             return done(null, false, { message: 'Incorrect password.' });
-//           }
-
-//           return done(null, user);
-//         });
-//       });
-//     }
-//   ));
-// };
--- server/web.js
+++ server/web.js
@@ -9,178 +9,166 @@ var http = require('http');
 var express = require('express');
 
 // We need to parse the params via form, using x-www-form-urlunencoded
+// Ahora estas librerías se cargan por separado desde express > 4.x
 var bodyParser = require('body-parser');
 
 // Needed in order to keep a session
+// Ahora estas librerías se cargan por separado desde express > 4.x
 var session = require('express-session');
 
+// Los mecanismos de login son manejados a través de la librería
+// "passport"
 var passport = require('passport');
+
+// Nuestro mecanismo de comprobacion de contraseña estará
+// en auth.js
 var authController = require('./controller/auth.js');
 
 // Needed as a connector for our DB in MongoDB
 var mongoose = require('mongoose');
 
 // Needed in order to avoid the Cross Domain or CORS
+// ES NECESARIO???
 var cors = require('cors');
 
-// Needed as session handler.
-var passport = require('passport');
 
-// We need 2 identities: "dongles" and "content" shared
+
+// Creamos nuestras identidades
 var userController = require('./controller/user.js');
+// SAMPLE: Hay que descomentar esto cuando esté listo
 // // var sampleManager = require('./models/sample.js');
+
+
+
+
+
 
 // Let's create the app based on 'express'
 var app = express();
-
-
 
 // As we will deploy in Heroku or similar, we will get the
 // port defined in the enviroment, or the 8080 by default
 var port = process.env.PORT || 8080;
 app.set('port', port);
 
-// // console.log(process.env.MONGOLAB_URI);
-
 // Connect with the DB. As before, we need to connect it with
 // the right ENV params, based on the platform to deploy.
 app.db = mongoose.connect(process.env.MONGOLAB_URI);
-
-
-
-// // Use the body-parser package in our application.
-// // NOTE! Before express 4.x, this could be declared directly as
-// // app.use(express.bodyParser());
-// app.use(bodyParser.urlencoded({
-//   extended: true
-// }));
-
-
-// app.use(
-//   session(
-//     {
-//       secret: process.env.COOKIEHASH || 'COOKIEHASH',
-//       cookie: {
-//         maxAge: 60000
-//       }
-//     }
-//   )
-// );
-
-
-// app.use(cors()); // Enable cors module
-
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-
 
 // Passport does not directly manage your session, it only uses the session.
 // So you configure session attributes (e.g. life of your session) via express
 var sessionOpts = {
   saveUninitialized: true, // saved new sessions
   resave: false, // do not automatically write to the session store
-  // store: sessionStore,
   secret: 'SECRET',
   cookie : { httpOnly: true, maxAge: 2419200000 } // configure when sessions expires
-}
+};
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
-// app.use(cookieParser('SECRET'))
-app.use(session(sessionOpts))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+// NOTA IMPORTANTE! Hay que establecer primero la sesion
+// de express y después la sesion de passport
+app.use(session(sessionOpts));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use(passport.initialize())
-app.use(passport.session())
+// COMPROBAR SI ES NECESARIO
+// app.use(cors()); // Enable cors module
 
 
+// Create our Express router. Está genial ya que podemos
+// redirigir nuestras peticiones tomando una base diferente
+// Ahora usamos /api/v1, pero si esto queremos que se modifique
+// en el futuro se puede hacer sólo modificando una línea.
 
-// Create our Express router
 var router = express.Router();
 
 router.all('*', function(req, res, next) {
-  console.log('Aquí iría la funcion que comprueba la sesion');
+  // Por cada petición imprimo su info.
+  console.log('*******************************************');
   console.log('req.app ' + req.app);
   console.log('req.originalUrl ' + req.originalUrl);
   console.log('req.params ' + JSON.stringify(req.params));
   console.log('req.body ' + req.body);
-
-  console.log('*******************************************');
   console.log('req.session ' + JSON.stringify(req.session));
   console.log('req.isAuthenticated ' + req.isAuthenticated());
   console.log('*******************************************');
   next();
 });
 
+// - ¿Tenemos una sesion?
+// - NO: Devolvemos al cliente una nota para que muestre el /login
+// o lo hacemos con templates aquí si finalmente tomamos esta decision
+// - SI: Permitimos ejecutar la consulta moviendonos al siguiente paso
+function isSessionAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    console.log('ESTAS AUTENTICADO PREVIAMENTE');
+    return next();
+  } else {
+    res.send('NO ESTAS AUTENTICADO');
+  }
+}
 
 router.route('/samples')
-  .get(function(req, res, next) {
-    if (req.isAuthenticated()) {
-      console.log('ESTAS AUTENTICADO PREVIAMENTE');
-      return next();
-    } else {
-      res.send('NO ESTAS AUTENTICADO');
+  .get(
+    isSessionAuthenticated,
+    function(req, res) {
+      res.send('GET SAMPLES de ' + req.session);
     }
-  }, function(req, res) {
-    res.send('Desde GET /samples hemos funcionado bien');
-  })
-  .post(function(req, res, next) {
-    if (req.isAuthenticated()) {
-      console.log('ESTAS AUTENTICADO PREVIAMENTE');
-      return next();
-    } else {
-      res.send('NO ESTAS AUTENTICADO');
+  )
+  .post(
+    isSessionAuthenticated,
+    function(req, res) {
+      res.send('POST SAMPLES de ' + req.session.passport.user.username);
     }
-  }, function(req, res) {
-    res.send('Desde POST /samples hemos funcionado bien');
-  });
+  );
 
-// router.route('/login')
-//   .post(authController.isAuthenticated, function(req, res) {
-//     if (req.isAuthenticated()) {
-//       res.send('ESTAS AUTENTICADO');
-//     } else {
-//       res.send('NO ESTAS AUTENTICADO');
-//     }
-
-//   });
+function isNotSessionAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    res.send('ESTAS AUTENTICADO PREVIAMENTE');
+  } else {
+    console.log('NO ESTAS AUTENTICADO');
+    return next();
+  }
+}
 
 
 router.route('/login')
-  .post(function(req, res, next) {
-    if (req.isAuthenticated()) {
-      res.send('ESTAS AUTENTICADO PREVIAMENTE');
-    } else {
-      console.log('NO ESTAS AUTENTICADO');
-      return next();
-    }
-  }, authController.isAuthenticated, function(req, res) {
+  .post(
+    // Compruebo si hay una sesion y, por lo tanto,
+    // no es necesario login
+    isNotSessionAuthenticated,
+    // Si no hay sesion, compruebo si username y password
+    // están registrados
+    authController.isAuthenticated,
+    // Si no están registrados, tendremos que llevarle
+    // a una página de error o de sign up
+    function(req, res) {
     if (req.isAuthenticated()) {
       res.send('AHORA ESTAS AUTENTICADO');
     } else {
       res.send('NO ESTAS AUTENTICADO');
     }
-
   });
-
-
 
 
 router.route('/logout')
-  .post(function(req, res) {
-    req.session.destroy(function(err) {
-      if (err) {
-        res.send('Error when /logout :(');
-      }
-      req.session = null; // Deletes the cookie.
-      res.session = null;
-      res.send('/logout !!!!');
-
-    })
-
-
-
-  });
+  .post(
+    // Compruebo si no hay una sesion y, por lo tanto,
+    // no es necesario logout
+    isSessionAuthenticated,
+    // Si está registrado, procedemos al logout
+    function(req, res) {
+      req.session.destroy(function(err) {
+        if (err) {
+          res.send('Error when /logout :(');
+        }
+        req.session = null; // Deletes the cookie.
+        res.session = null;
+        res.send('LOGOUT con éxito');
+      });
+    }
+  );
 
 router.route('/register')
   .post(userController.register);
